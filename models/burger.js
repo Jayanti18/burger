8adbb7f86b9d8077dddc056abd0c8f0926db5165
jayanti18 <jayantirajesh@yahoo.com>
2018-10-11 16:54:10 -0400

    updating Eat-the-Burger app project.

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(bu) {
+  all: function(cb) {
     orm.all("burgers", function(res) {
-      bu(res);
+      cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, bu) {
+  create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       bu(res);
     });
   },
-  update: function(objColVals, condition, bu) {
+  update: function(objColVals, condition, cb) {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
